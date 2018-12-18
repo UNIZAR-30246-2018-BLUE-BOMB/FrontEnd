8b8d0bdc97f9e71c9644659ae8c7d44811caf479
AbelChT <abelcht@outlook.es>
2018-12-18 21:27:35 +0100

    ads done

--- src/app/redirect-page/redirect-page.component.ts
+++ src/app/redirect-page/redirect-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {DomSanitizer,SafeResourceUrl} from '@angular/platform-browser';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
 
 @Component({
   selector: 'app-redirect-page',
@@ -8,12 +11,59 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RedirectPageComponent implements OnInit {
 
-  shortenID: String = ""
+  stompClient = null;
 
-  constructor(private route: ActivatedRoute) { }
+  shortenID: String = "";
+
+  adsPage: SafeResourceUrl;
+
+  timeoutRedirect: Number = 0;
+
+  constructor(private route: ActivatedRoute, public sanitizer:DomSanitizer) { }
 
   ngOnInit() {
     this.shortenID = this.route.snapshot.paramMap.get('id');
+    this.connect(this.shortenID);
   }
 
+  private connect(sequence: String) {
+    const socket = new SockJS('http://localhost:8080/ws');
+    this.stompClient = Stomp.over(socket);
+    var self = this;
+    this.stompClient.connect({}, function (frame) {
+      console.log('Connected: ' + frame);
+
+      self.stompClient.subscribe('/user/queue/error/info', function (error) {
+        console.log(error.body);
+        // TODO: What do when errors come?
+      });
+
+      self.stompClient.subscribe('/user/info/' + sequence, function (message) {
+        let headURL = JSON.parse(message.body).headURL;
+        let interstitialURL = JSON.parse(message.body).interstitialURL;
+        let secondsToRedirect = JSON.parse(message.body).secondsToRedirect;
+
+        if(interstitialURL!=null && interstitialURL!=""){
+          self.adsPage = self.sanitizer.bypassSecurityTrustResourceUrl(interstitialURL);
+          self.timeoutRedirect = secondsToRedirect;
+        }else if(headURL!=null && headURL!=""){
+          self.redirectToURL(headURL);
+        }
+      });
+
+      self.stompClient.send('/app/info', {}, sequence);
+    });
+  }
+
+  private disconnect() {
+    if (this.stompClient !== null) {
+      this.stompClient.disconnect();
+    }
+    console.log('Disconnected');
+  }
+
+  private redirectToURL(newURL: string){
+    this.disconnect();
+   window.location.href = newURL;
+  }
 }
